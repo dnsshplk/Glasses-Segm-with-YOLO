890130d01d1cc3895c10a054d2f51c49ca35a03d
dnsshplk <denis0710shpilka@gmail.com>
2024-06-07 18:21:28 +0300

    init commit

---
+++ inference.py
@@ -0,0 +1,58 @@
+import os
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+
+def main():
+    model = YOLO(os.path.join('run', 'best.pt')) 
+
+    images_dir = os.path.join('test', 'images')
+    actual_mask_dir = os.path.join('test', 'masks_actual')
+    output_dir = os.path.join('test', 'masks_model')
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    for image_file in os.listdir(images_dir):
+        image_path = os.path.join(images_dir, image_file)
+
+        image = cv2.imread(image_path)
+
+        results = model.predict(image)
+
+        masks = results[0].masks   
+            
+        masks = results[0].masks.data.cpu().numpy() 
+        classes = results[0].boxes.cls.cpu().numpy()  
+
+        mask_glasses = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
+        mask_lenses = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
+            
+        for mask, cls in zip(masks, classes):
+            mask_uint8 = (mask * 255).astype(np.uint8)
+            if cls == 0:  
+                mask_glasses = np.maximum(mask_glasses, mask_uint8)
+            elif cls == 1:  
+                mask_lenses = np.maximum(mask_lenses, mask_uint8)
+            
+        result_mask = mask_glasses - mask_lenses
+            
+        result_mask = cv2.cvtColor(result_mask, cv2.COLOR_GRAY2BGR)
+
+
+        mask_output_path = os.path.join(output_dir, f"{image_file}_mask.png")
+
+        actual_mask_path = os.path.join(actual_mask_dir, f'{image_file[:8]}mask.png')
+        actual_mask = cv2.imread(actual_mask_path)
+
+        actual_mask = cv2.resize(actual_mask, (512, 512))
+        # img_output_path = os.path.join(output_dir, image_file)
+        
+        result_image = np.concatenate((image, actual_mask, result_mask), axis = 1)
+
+        cv2.imwrite(mask_output_path, result_image)
+        # cv2.imwrite(img_output_path, image)
+
+
+if __name__ == '__main__':
+    main()
